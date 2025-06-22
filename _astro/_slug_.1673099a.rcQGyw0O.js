b63cee2da55fc8cb4bddffb69a2144e2494e23e1
migiwa-ya <contact@migiwa-ya.com>
2025-06-22 17:40:12 +0900

    Updates

---
+++ _astro/_slug_.1673099a.rcQGyw0O.js
@@ -0,0 +1,1 @@
+const n="_card_1afhb_1",t="_date_1afhb_35",o="_content_1afhb_77",e={card:n,date:t,content:o},s="_loadingFooter_j4v6d_1",a="_spinner_j4v6d_6",_={loadingFooter:s,spinner:a};export{_ as a,e as s};
